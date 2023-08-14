178d62ae9644563dca8a4c2e0eb732225b3960bf
Georgios Drosinos <gdrosin@gmail.com>
2023-08-14 20:29:22 +0200

    |5 kyu| weight for weight

--- JavaScript/5kyu__weight_for_weight.js
+++ JavaScript/5kyu__weight_for_weight.js
@@ -3,9 +3,11 @@ Title: Weight for weight
 Rank: 5 kyu
 
 Description:
-My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with the weights of members is published and each month he is the last on the list which means he is the heaviest.
+My friend John and I are members of the "Fat to Fit Club (FFC)". John is worried because each month a list with 
+the weights of members is published and each month he is the last on the list which means he is the heaviest.
 
-I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list". It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
+I am the one who establishes the list so I told him: "Don't worry any more, I will modify the order of the list".
+It was decided to attribute a "weight" to numbers. The weight of a number will be from now on the sum of its digits.
 
 For example 99 will have "weight" 18, 100 will have "weight" 1 so in the list 100 will come before 99.
 
