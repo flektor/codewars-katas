2893dc44d2ecbf200d4366970b5f6e04fbf7b969
Georgios Drosinos <gdrosin@gmail.com>
2023-08-14 20:31:58 +0200

    |7 kyu| most sales

--- JavaScript/7kyu__most_sales.js
+++ JavaScript/7kyu__most_sales.js
@@ -3,14 +3,16 @@ Title: Most sales
 Rank: 7 kyu
 
 Description:
-You work in the best consumer electronics corporation, and your boss wants to find out which three products generate the most revenue. Given 3 lists of the same length like these:
+You work in the best consumer electronics corporation, and your boss wants to find out
+which three products generate the most revenue. Given 3 lists of the same length like these:
 
 products: ["Computer", "Cell Phones", "Vacuum Cleaner"]
 amounts: [3, 24, 8]
 prices: [199, 299, 399]
 return the three product names with the highest revenue (amount * price).
 
-Note: if multiple products have the same revenue, order them according to their original positions in the input list.
+Note: if multiple products have the same revenue, order them according to their original 
+positions in the input list.
 
 */
 
