970ad084eae0c6d57570ee9faae2fe7e95b5ddf7
flektor <gdrosin@gmail.com>
2023-08-14 20:21:58 +0200

    |5 kyu| calculating with functions

--- JavaScript/5kyu__calculating_with_functions.js
+++ JavaScript/5kyu__calculating_with_functions.js
@@ -15,7 +15,7 @@ There must be a function for each number from 0 ("zero") to 9 ("nine")
 There must be a function for each of the following mathematical operations: plus, minus, times, dividedBy
 Each calculation consist of exactly one operation and two numbers
 The most outer function represents the left operand, the most inner function represents the right operand
-Division should be integer division. For example, this should return 2, not 2.666666...:
+Division should be integer division. For example, this should return 2, not 2.666666..:
 eight(dividedBy(three()));
 
 #FUNCTIONAL PROGRAMMING
