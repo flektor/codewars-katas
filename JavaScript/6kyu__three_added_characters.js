5437c862341a121fd2d99aaded4bd96f723bcd7f
flektor <gdrosin@gmail.com>
2023-08-17 22:56:05 +0200

    |6 kyu| three added characters

--- JavaScript/6kyu__three_added_characters.js
+++ JavaScript/6kyu__three_added_characters.js
@@ -3,7 +3,9 @@ Title: Three added Characters
 Rank: 6 kyu
 
 Description:
-Given two strings, the first being a random string and the second being the same as the first, but with three added characters somewhere in the string (three same characters),
+Given two strings, the first being a random string and the second being
+the same as the first, but with three added characters somewhere in the
+string (three same characters),
 
 Write a function that returns the added character
 
@@ -12,7 +14,8 @@ string1 = "hello"
 string2 = "aaahello"
 
 // => 'a'
-The above is just an example; the characters could be anywhere in the string and string2 is actually shuffled.
+The above is just an example; the characters could be anywhere in the
+string and string2 is actually shuffled.
 
 Another example
 string1 = "abcde"
@@ -25,9 +28,11 @@ string1 = "aabbcc"
 string2 = "aacccbbcc"
 
 // => 'c'
-You can assume that string2 will aways be larger than string1, and there will always be three added characters in string2.
+You can assume that string2 will aways be larger than string1, and there
+will always be three added characters in string2.
 
-Write the function addedChar() that takes two strings and return the added character as described above.
+Write the function addedChar() that takes two strings and return the
+added character as described above.
 
 #LOGIC, #STRINGS
 */
