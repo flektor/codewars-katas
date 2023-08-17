a5b8567ad5b8733604671fe6fded6e6a3018fe17
flektor <gdrosin@gmail.com>
2023-08-17 22:56:44 +0200

    |6 kyu| write number in expanded form

--- JavaScript/6kyu__write_number_in_expanded_form.js
+++ JavaScript/6kyu__write_number_in_expanded_form.js
@@ -4,7 +4,8 @@ Rank: 6 kyu
 
 Description:
 Write Number in Expanded Form
-You will be given a number and you will need to return it as a string in Expanded Form. For example:
+You will be given a number and you will need to return it as a
+string in Expanded Form. For example:
 
 expandedForm(12); // Should return '10 + 2'
 expandedForm(42); // Should return '40 + 2'
